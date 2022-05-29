677d1cab2eef354f2fdaf33820483a56e8a591b5
Aniket <aniketpawar0351@gmail.com>
2022-05-29 23:08:37 +0530

    ...

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const port=process.env.PORT || 5000;
+const port= 5000;
 const databaseURL="mongodb://localhost:27017/sensor_data";
 
 app.listen(port,()=>console.log("hellow word"));
