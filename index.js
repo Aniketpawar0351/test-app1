e306cecdaed6145ddeac8f23f8cee9e7758b5798
Aniket <aniketpawar0351@gmail.com>
2022-05-30 00:17:54 +0530

    mongo atlas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 const port=process.env.PORT || 3000;
-const databaseURL="mongodb://localhost:27017/sensor_data";
+const databaseURL="mongodb+srv://cn_grp3:cn_grp3@cluster0.bf5bawq.mongodb.net/sensors_data";
 
 app.listen(port,()=>console.log("hellow word"));
 
@@ -24,7 +24,7 @@ app.get('/sensor_data/:api_key/:s_data',(req,res)=>{
     console.log(s_data);
 
         //res.send("hello"+s_data);
-        //myfun.createDoc(s_data);
+        myfun.createDoc(s_data);
         res.status(201).send("hello");
         console.log(req.params);
     
