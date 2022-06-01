b9c45f3e772fa5789e2ffa64df4df0cf2e62113e
Aniket <aniketpawar0351@gmail.com>
2022-06-01 17:38:06 +0530

    frontend added

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const port=process.env.PORT || 3000;
 const databaseURL="mongodb+srv://cn_grp3:cn_grp3@cluster0.bf5bawq.mongodb.net/sensors_data";
 
-app.listen(port,()=>console.log("hellow word"));
+app.listen(port,()=>console.log("hello world"));
 
-
+var level1=0;
+var level2=0;
 
 
 import connectDB from './connectdb.js';
@@ -13,19 +14,26 @@ import './s_data.js';
 import * as myfun from './s_data.js';
 connectDB(databaseURL);
 
-
+app.set('view engine', 'ejs');
+import {join} from 'path';
 app.get('/',(req, res)=>{
-    res.status(200).send("ok");
-    console.log("hello");
+    const result1=myfun.all_records(); 
+    result1.then((value)=>{
+            const t1=value[0].level1;
+            const t2=value[0].level2;
+           console.log(value[0].level1);
+           console.log(value[0].level2);
+           res.render('pages/index',{tl1:t1,tl2:t2
+         })
+    });
 });
 
-app.get('/sensor_data/:api_key/:s_data',(req,res)=>{
-    const {api_key,s_data}=req.params;
-    console.log(s_data);
-
-        //res.send("hello"+s_data);
-        myfun.createDoc(s_data);
-        res.status(201).send("hello");
+app.get('/sensor_data/:api_key/:tank1/:tank2',(req,res)=>{
+    const {api_key,tank1,tank2}=req.params;
+    console.log(tank1,tank2);
+    if(api_key=='cn_grp3'){
+        myfun.createDoc(tank1,tank2);
+        res.status(200).send("hello");
         console.log(req.params);
-    
+    }
 });
--- s_data.js
+++ s_data.js
@@ -3,22 +3,31 @@ import mongoose from 'mongoose';
 //defining schema
 const sensor_dataSchema = new mongoose.Schema({
     sdate:{type:Date, default: Date.now},
-    level:{type:Number}
+    level1:{type:Number},
+    level2:{type:Number}
 })
+export let tl1;
+export let tl2;
 
 //compiling Shema
-const sensor_data_model = mongoose.model('sensors_data',sensor_dataSchema)
+export const sensor_data_model = mongoose.model('sensors_data',sensor_dataSchema)
 
 export const all_records= async ()=>{
-    const result=await sensor_data_model.find();
-    console.log(result);
-    //return result;
+    try{
+        const result=await sensor_data_model.find().sort({_id:-1}).limit(1);
+        console.log(result);
+        return result;
+
+    }catch(error){
+        console.log(error);
+    }
 }
 //all_records();
-export const createDoc = async(s_data)=>{
+export const createDoc = async(tank1,tank2)=>{
     try{
         const new_sensor_data = new sensor_data_model({
-            level:s_data
+            level1:tank1,
+            level2:tank2
         })
        const result = await sensor_data_model.insertMany([new_sensor_data]);
        console.log(result);
