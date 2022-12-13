8bdcc96b61b017f9c5b3a2d104b172c442dbed48
Aniket <aniketpawar0351@gmail.com>
2022-12-13 18:08:22 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 const app = express();
-const port=process.env.PORT || 3000;
-const databaseURL="mongodb+srv://cn_grp3:cn_grp3@cluster0.bf5bawq.mongodb.net/sensors_data";
+// const port=process.env.PORT || 3000;
+const port=3000;
+// const databaseURL="mongodb://localhost:27017/vehicle_data";
+const databaseURL="mongodb+srv://cn_grp3:cn_grp3@cluster0.bf5bawq.mongodb.net/?retryWrites=true&w=majority";
 
 app.listen(port,()=>console.log("hello world"));
 
-var level1=0;
-var level2=0;
 
 
 import connectDB from './connectdb.js';
@@ -16,30 +16,81 @@ connectDB(databaseURL);
 
 app.set('view engine', 'ejs');
 import {join} from 'path';
+import { checkPrime } from 'crypto';
+
+app.use(express.urlencoded({extended:false}));
+
 app.get('/',(req, res)=>{
-    const result1=myfun.all_records(); 
+    //const result1=myfun.all_records1(res); 
+        res.render("pages/index");
+    
+});
+app.get('/recentrecord',(req, res)=>{
+    const result1=myfun.all_records1(res); 
     result1.then((value)=>{
-            const t1=Math.round((value[0].level1)/2.4);
-            const t2=Math.round((value[0].level2)/2.4);
-           console.log(value[0].level1);
-           console.log(value[0].level2);
-           res.render('pages/index',{tl1:t1,tl2:t2
-         })
-    });
+        console.log(value);
+        //res.send(value);
+ });
+    
+});
+app.get('/allrecord',(req, res)=>{
+    const result1=myfun.all_records3(res); 
+    result1.then((value)=>{
+        console.log(value);
+        //res.send(value);
+        
+ });
+    
 });
 
-app.get('/update',(req,res)=>{
-    const result1=myfun.all_records(); 
+app.get('/unrecognized',(req, res)=>{
+    const result1=myfun.all_records2(res); 
     result1.then((value)=>{
-           res.send(value);
-    });
+        console.log(value);
+        //res.send(value);
+       
+ });
+    
+});
+
+app.get('/newvehicle',(req, res)=>{
+    const result1=myfun.all_records1(); 
+    result1.then((value)=>{
+        //console.log(value);
+        //res.send(value);
+        res.render("pages/new_vehicle");
+ });
+    
+});
+
+app.post('/newvehicle_submited',(req,res)=>{
+    myfun.createDoc3(req.body.uname,req.body.state,req.body.vnumber,req.body.email,req.body.bphone_no);
+    console.log(req.body);
+    res.render("pages/gg");
 })
-app.get('/sensor_data/:api_key/:tank1/:tank2',(req,res)=>{
-    const {api_key,tank1,tank2}=req.params;
-    console.log(tank1,tank2);
-    if(api_key=='cn_grp3'){
-        myfun.createDoc(tank1,tank2);
+app.get('/vehicle_data/:api_key/:number_p/:speed',(req,res)=>{
+    const {api_key,number_p,speed}=req.params;
+    //console.log(number_p,speed);
+    if(api_key=="IC_C"){
+        if(myfun.check(number_p))
+        {
+            console.log("yes")
+            myfun.createDoc1(number_p,speed);
+        }
+        else
+        {
+            myfun.createDoc2(number_p,speed);
+        }
+        
         res.status(200).send("hello");
-        console.log(req.params);
+        //console.log(req.params);
     }
 });
+
+// <% data.forEach((item)=>{ %>
+//     <tr>
+//       <td><%= item.number_p %></td>
+//       <td><%= item.speed %></td>
+//       <td><%= item.sdate %></td>
+//     </tr>
+//  <% }) %>
--- s_data.js
+++ s_data.js
@@ -1,39 +1,124 @@
 import mongoose from 'mongoose';
 
 //defining schema
-const sensor_dataSchema = new mongoose.Schema({
+const vehicle_dataSchema1 = new mongoose.Schema({
     sdate:{type:Date, default: Date.now},
-    level1:{type:Number},
-    level2:{type:Number}
+    number_p:{type:String},
+    speed:{type:String}
 })
-export let tl1;
-export let tl2;
+const vehicle_dataSchema2 = new mongoose.Schema({
+    unrdate:{type:Date, default: Date.now},
+    unrnumber_p:{type:String},
+    unrspeed:{type:String}
+})
+const vehicle_dataSchema3 = new mongoose.Schema({
+    uname:{type:String},
+    state:{type:String},
+    unumber_p:{type:String},
+    uemail:{type:String},
+    phone:{type:String},
+    sdate:{type:Date, default: Date.now}
+})
+// export let tl1;
+// export let tl2;
 
 //compiling Shema
-export const sensor_data_model = mongoose.model('sensors_data',sensor_dataSchema)
+export const vehicle_data_model1 = mongoose.model('vehicle_datas',vehicle_dataSchema1)
+export const vehicle_data_model2 = mongoose.model('unr_vehicle_data',vehicle_dataSchema2)
+export const vehicle_data_model3 = mongoose.model('user_vehicle_data',vehicle_dataSchema3)
 
-export const all_records= async ()=>{
+export const all_records1= async (res)=>{
     try{
-        const result=await sensor_data_model.find().sort({_id:-1}).limit(1);
-        // console.log(result);
-        return result;
+        const result=await vehicle_data_model1.find().limit(5);
+        //console.log(result);
+        res.render("pages/recent_record",{data: result});
+        return {result};
 
     }catch(error){
         console.log(error);
     }
 }
-//all_records();
-export const createDoc = async(tank1,tank2)=>{
+
+
+export const all_records2= async (res)=>{
     try{
-        const new_sensor_data = new sensor_data_model({
-            level1:tank1,
-            level2:tank2
+        const result=await vehicle_data_model2.find();
+        //console.log(result);
+        res.render("pages/unrecognized_vehicle",{data: result});
+        return {result};
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const all_records3= async (res)=>{
+    try{
+        const result=await vehicle_data_model3.find();
+        //console.log(result);
+        res.render("pages/all_record",{data: result});
+        return {result};
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+//all_records();
+export const createDoc1 = async(var1,var2)=>{
+    try{
+        const new_vehicle_data1 = new vehicle_data_model1({
+            number_p:var1,
+            speed:var2
         })
-       const result = await sensor_data_model.insertMany([new_sensor_data]);
+       const result = await vehicle_data_model1.insertMany([new_vehicle_data1]);
+       //console.log(result);
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const createDoc2 = async(var1,var2)=>{
+    try{
+        const new_vehicle_data2 = new vehicle_data_model2({
+            unrnumber_p:var1,
+            unrspeed:var2
+        })
+       const result = await vehicle_data_model2.insertMany([new_vehicle_data2]);
+       //console.log(result);
+
+    }catch(error){
+        console.log(error);
+    }
+}
+
+export const createDoc3 = async(var1,var2,var3,var4,var5)=>{
+    try{
+        const new_vehicle_data3 = new vehicle_data_model3({
+            uname:var1,
+            state:var2,
+            unumber_p:var3,
+            uemail:var4,
+            phone:var5
+        })
+       const result = await vehicle_data_model3.insertMany([new_vehicle_data3]);
        console.log(result);
     }catch(error){
         console.log(error);
     }
 }
 
+
+export function check(var1){
+    const result= vehicle_data_model3.find({unumber_p: var1});
+    console.log(result);
+    if(result.length===0)
+    {
+        console.log("NI")
+        return true;
+    }
+    else return false;
+}
 
